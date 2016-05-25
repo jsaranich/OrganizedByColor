dd80359ed0fc55283f1372c90246a02b78b3f955
jsaranich <retailjess@gmail.com>
2016-05-25 11:52:32 -0400

    two more controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "about_us" => 'pages#about_us'
+
+  get "contact_us" => 'pages#contact_us'
+
   resources :books
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
